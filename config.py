8cacbc8dcd3856ec4e1b1be5e4a8e2755dbfb004
MiracleKAI <1531996166@qq.com>
2019-03-28 19:33:22 +0800

    ww

--- config.py
+++ config.py
@@ -54,3 +54,4 @@ def get_jsonData(filename):
 if __name__ == "__main__":
     # modifyConfig("dada.conf", "config")
     account = getConfig("dada.conf", "account")
+    print("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS")
